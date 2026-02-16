ffb3522408f25146f05c655189ea1344e0ffcd71
YANG JIN <yanggyin3@outlook.com>
2026-02-17 00:32:37 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -838,7 +838,7 @@ install_x-ui() {
         min_version="2.3.5"
         
         if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]]; then
-            echo -e "${red}Please use a newer version (at least v2.3.5). Exiting installation.${plain}"
+            
             exit 1
         fi
         
